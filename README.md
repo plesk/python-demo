80864cfc3a73a41e19dbc0c90966efaa40e9d541
Alexey Yuzhakov <sibprogrammer@gmail.com>
2026-08-01 21:48:44 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python Demo App
+
+A simple demo project on [Flask](https://flask.palletsprojects.com/) to test Plesk [Python](https://www.python.org/) support.
---
+++ main.py
@@ -0,0 +1,16 @@
+import platform
+from importlib.metadata import version
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return (
+        f"Hello, World!<br>"
+        f"Python {platform.python_version()}<br>"
+        f"Flask {version('flask')}"
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
